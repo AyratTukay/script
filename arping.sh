3b848f567e7e49bc98eb1bc38466de6ad08b2deb
Ayrat <165489142+AyratTukay@users.noreply.github.com>
2025-08-03 00:10:29 +0300

    Update arping.sh

--- arping.sh
+++ arping.sh
@@ -1,18 +1,63 @@
 #!/bin/bash
 PREFIX="${1:-NOT_SET}"
 INTERFACE="$2"
+SUBNET="$3"
+HOST="$4"
 
-[[ "$PREFIX" = "NOT_SET" ]] && { echo "\$PREFIX must be passed as first positional argument"; exit 1; }
-if [[ -z "$INTERFACE" ]]; then
-    echo "\$INTERFACE must be passed as second positional argument"
-    exit 1
+checking_host() {
+	for HOST in {0..255}
+		do
+			echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST}"
+			arping -c 3 -i "$INTERFACE" "$PREFIX"".""$SUBNET"".""${HOST}" 2> /dev/null
+		done
+}
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
 fi
-
-for SUBNET in {1..255}
-do
-	for HOST in {1..255}
+[[ "$PREFIX" = "NOT_SET" ]] && { echo "\$PREFIX должен быть передан как первый позиционный аргумент"; exit 1; }
+if [[ -z "$INTERFACE" ]]; then
+    echo "\$INTERFACE должен быть передан как второй позиционный аргумент"
+    exit 1
+elif [[ -z "$SUBNET" && -z "$HOST" ]]; then
+	for SUBNET in {0..255}
 	do
-		echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST}"
-		arping -c 3 -i "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST}" 2> /dev/null
+		checking_host
 	done
-done
+elif [[ -z "$HOST" ]]; then
+	checking_host
+else 
+	arping -c 3 -i "$INTERFACE" "$PREFIX"".""$SUBNET"".""${HOST}" 2> /dev/null
+fi#!/bin/bash
+PREFIX="${1:-NOT_SET}"
+INTERFACE="$2"
+SUBNET="$3"
+HOST="$4"
+
+checking_host() {
+	for HOST in {0..255}
+		do
+			echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST}"
+			arping -c 3 -i "$INTERFACE" "$PREFIX"".""$SUBNET"".""${HOST}" 2> /dev/null
+		done
+}
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+[[ "$PREFIX" = "NOT_SET" ]] && { echo "\$PREFIX должен быть передан как первый позиционный аргумент"; exit 1; }
+if [[ -z "$INTERFACE" ]]; then
+    echo "\$INTERFACE должен быть передан как второй позиционный аргумент"
+    exit 1
+elif [[ -z "$SUBNET" && -z "$HOST" ]]; then
+	for SUBNET in {0..255}
+	do
+		checking_host
+	done
+elif [[ -z "$HOST" ]]; then
+	checking_host
+else 
+	arping -c 3 -i "$INTERFACE" "$PREFIX"".""$SUBNET"".""${HOST}" 2> /dev/null
+fi
